899ab4a5b5623fa01d4848556032b867e0d04779
ayanshuDS <73418468+MET4L-DS@users.noreply.github.com>
2024-05-31 01:30:54 +0530

    implemented RAG using FAISS

--- controllers/context.js
+++ controllers/context.js
@@ -1,29 +1,36 @@
 import { HuggingFaceInferenceEmbeddings } from "@langchain/community/embeddings/hf";
 
-import { Pinecone } from "@pinecone-database/pinecone";
-import { PineconeStore } from "@langchain/pinecone";
+// import { Pinecone } from "@pinecone-database/pinecone";
+// import { PineconeStore } from "@langchain/pinecone";
+
+import { FaissStore } from "@langchain/community/vectorstores/faiss";
 
 const getContext = async (req, res) => {
     const { prompt } = req.body;
 
     try {
         console.log(prompt);
-        const pinecone = new Pinecone({
-            apiKey: "9b0c2770-8107-48e3-9b4a-b12f71b88372",
-        });
+        // const pinecone = new Pinecone({
+        //     apiKey: "9b0c2770-8107-48e3-9b4a-b12f71b88372",
+        // });
 
-        const pineconeIndex = pinecone.Index("law-gpt-embeddings");
+        // const pineconeIndex = pinecone.Index("law-gpt-embeddings");
 
         const embeddings = new HuggingFaceInferenceEmbeddings({
             apiKey: "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", // In Node.js defaults to process.env.HUGGINGFACEHUB_API_KEY
-            model: "intfloat/multilingual-e5-large",
+            // model: "intfloat/multilingual-e5-large",
+            model: "Muennighoff/SGPT-125M-weightedmean-msmarco-specb-bitfit",
         });
 
-        const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
-            pineconeIndex,
-        });
+        const directory = "./vector-store";
 
-        const data = await vectorStore.similaritySearch(prompt, 10);
+        const vectorStore = await FaissStore.load(directory, embeddings);
+
+        // const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
+        //     pineconeIndex,
+        // });
+
+        const data = await vectorStore.similaritySearch(prompt, 5);
 
         console.log(data);
         res.status(200).json({ prompt: prompt, data: data });
