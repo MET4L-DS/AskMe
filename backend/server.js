67f6e38691250aa7ebe4d7f1d053db09cdfa8284
ayanshuDS <73418468+MET4L-DS@users.noreply.github.com>
2024-06-20 00:39:53 +0530

    Moved project to frontend and backend subdirectories

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,17 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
 import contextRouter from "./routes/context.js";
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 
 app.use(express.json());
 app.use("/api/v1", contextRouter);
-app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+	console.log(`Server listening at http://localhost:${PORT}`);
 });
