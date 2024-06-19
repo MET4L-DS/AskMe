0c2058c01bb971aba80af586a58c87f49af1691e
ayanshuDS <73418468+MET4L-DS@users.noreply.github.com>
2024-06-20 02:04:21 +0530

    updated cors settings

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,13 @@ import contextRouter from "./routes/context.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(
+	cors({
+		origin: "https://law-gpt-context.vercel.app/",
+		methods: ["GET", "POST"],
+		credentials: true,
+	})
+);
 
 app.use(express.json());
 app.use("/api/v1", contextRouter);
